f6b9ff8aef14b5509751560c4884c6cfc12fee40
GaneshGanta <ganeshsagar373@gmail.com>
2023-01-05 10:10:24 +0530

    repositories

---
+++ Mini-Project/src/main/java/com/ecommerce/repository/CartDao.java
@@ -0,0 +1,15 @@
+package com.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.onestore.exception.ProductException;
+import com.onestore.model.Cart;
+
+@Repository
+public interface CartDao extends JpaRepository<Cart, Integer>{
+   
+
+	
+}
